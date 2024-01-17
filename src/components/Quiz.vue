fab7a5b80f70fd480a3e4ff40fbd2b69825486a9
Lucas Dauto <lucasdauto@gmail.com>
2024-01-17 16:04:52 -0300

    simulate

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -2,7 +2,17 @@
     import { ref } from 'vue';
 
     const loading = ref(false)
+    const result = ref(null)
     
+    const processForm = async (formData) => {
+    // Simule uma chamada assíncrona
+    loading.value = true;
+    setTimeout(() => {
+      // Lógica para processar os dados do formulário e obter resultados
+      result.value = `Resultados para disciplina: ${formData.schoolSubjects}, Nível de dificuldade: ${formData.difficultyLevel}`;
+      loading.value = false;
+    }, 2000); // Simulando uma resposta após 2 segundos
+};
 </script>
 
 <template>
