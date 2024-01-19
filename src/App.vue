242a2e746ac2e937913d14f13894f13ac4c518d0
Lucas Dauto <lucasdauto@gmail.com>
2024-01-19 14:26:58 -0300

    Config layout default

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
+  <div
+    class="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500"
+  >
     <router-view></router-view>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
