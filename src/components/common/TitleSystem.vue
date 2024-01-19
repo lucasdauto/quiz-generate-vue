4fdf1c3380677c2cd7a5c045d22cb71c3bccda5e
Lucas Dauto <lucasdauto@gmail.com>
2024-01-19 14:27:22 -0300

    title default

---
+++ src/components/common/TitleSystem.vue
@@ -0,0 +1,9 @@
+<template>
+  <!-- Título -->
+  <h1
+    class="text-center text-5xl font-bold text-white drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] shadow-black mb-5"
+  >
+    Questões Inteligentes
+    <br />Adaptativas
+  </h1>
+</template>
